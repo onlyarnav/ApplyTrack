a46ce4e84449a10aca07d3ae11016279521dd5c0
Arnav <a.purohit1903@gmail.com>
2026-06-04 23:40:33 +0530

    Updated backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ import logging
 from fastapi import FastAPI
 
 from app.routes.auth import router as auth_router
+from app.routes.job_application import router as job_application_router
 
 logger = logging.getLogger(__name__)
 app = FastAPI(
@@ -11,6 +12,7 @@ app = FastAPI(
     version="1.0.0"
 )
 app.include_router(auth_router)
+app.include_router(job_application_router)
 
 
 @app.get("/")
